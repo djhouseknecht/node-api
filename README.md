384d702dfa135ceff8b8944a6065545c3530c1b8
David Houseknecht <david.j.houseknecht@gmail.com>
2022-03-29 20:46:17 -0400

    adding README and some example logic

---
+++ README.md
@@ -0,0 +1,7 @@
+# TODO
+* create `POST /api/search`
+  * you will need Postman or something to test the request
+  * use axios
+  * put the axios logic in a separate file and export
+
+* switch to typescript
--- app.js
+++ app.js
@@ -4,10 +4,18 @@ const app = express();
 const https = require('https');
 const basicAuth = require('express-basic-auth')
 const path = require('path');
+const axios = require('axios').default;
+
 let username = 'api_key_gXSBls3wFCSFwGrokZENaNtN';
+axios.defaults.auth = {
+  username,
+  password: ''
+};
+axios.defaults.baseURL = 'https://api.arkowl.com'
+
 const url = 'api.arkowl.com';
 let q_res = {
-    body1: ""
+  body1: ""
 }
 let email;
 let phone;
@@ -22,91 +30,117 @@ app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
-async function search() {
-    if (email != "" && phone != "") {
-        fullURL = extCOMB1 + email + extCOMB2 + phone + '/';
-    } else if (email != "" && phone === "") {
-        fullURL = extensionEMAIL + email + '/';
-    } else if (email === "" && phone != "") {
-        fullURL = extensionPHONE + phone + '/';
-    } else fullURL = "/v2";
-    console.log(url + fullURL);
 
-    var options = {
-        host: url,
-        port: 443,
-        path: fullURL,
-        // authentication headers
-        headers: {
-            'Authorization': 'Basic ' + new Buffer(username + ':').toString('base64')
-        }
-    };
-    //this is the call
-    request = https.get(options, function(res) {
-        q_res.body1body1 = "";
-        res.on('data', function(data) {
-            q_res.body1 += data;
-        });
-        res.on('end', function() {
-            //here we have the full response, html or json object
-            console.log(q_res.body1);
-        })
-        res.on('error', function(e) {
-            console.log("Got error: " + e.message);
-        });
+async function search () {
+  if (email != "" && phone != "") {
+    fullURL = extCOMB1 + email + extCOMB2 + phone + '/';
+  } else if (email != "" && phone === "") {
+    fullURL = extensionEMAIL + email + '/';
+  } else if (email === "" && phone != "") {
+    fullURL = extensionPHONE + phone + '/';
+  } else fullURL = "/v2";
+  console.log(url + fullURL);
+
+  var options = {
+    host: url,
+    port: 443,
+    path: fullURL,
+    // authentication headers
+    headers: {
+      'Authorization': 'Basic ' + new Buffer(username + ':').toString('base64')
+    }
+  };
+  //this is the call
+  return new Promise((resolve, reject) => {
+    https.get(options, function (res) {
+      q_res.body1body1 = "";
+      res.on('data', function (data) {
+        q_res.body1 += data;
+      });
+      res.on('end', function () {
+        //here we have the full response, html or json object
+        console.log(q_res.body1);
+        resolve();
+      })
+      res.on('error', function (e) {
+        console.log("Got error: " + e.message);
+        reject(e);
+      });
     });
-    //return q_res.body1;
+  });
+
+  //return q_res.body1;
 };
-app.get('/', function(req, res) {
-    q_res.body1 = "";
-    //res.sendFile(__dirname + '/training.html');
-    res.render("index.pug");
+
+function buildSearchUri (email, phone) {
+
+  return ''; // arkowl url to request
+}
+
+// TODO: make this a POST request
+app.get('/', function (req, res) {
+  q_res.body1 = "";
+  //res.sendFile(__dirname + '/training.html');
+  res.render("index.pug");
 });
-app.post('/', async function(req, res) {
-    q_res.body1 = "";
-    email = req.body.email;
-    phone = req.body.phone;
-    console.log(email);
-    console.log(phone);
-    //I cant make redirect or render wait till im done with the api call
-    await search();
-    try {
-        //res.send(`Email: ${email} Phone: ${phone}`);
-        setTimeout(() => {
-            res.redirect('/result');
-        }, 2000);
-    } catch (error) {
-        console.log("Error");
-    }
+app.post('/', async function (req, res) {
+  q_res.body1 = "";
+  email = req.body.email;
+  phone = req.body.phone;
+  console.log(email);
+  console.log(phone);
+  //I cant make redirect or render wait till im done with the api call
+  await search();
+  try {
+    //res.send(`Email: ${email} Phone: ${phone}`);
+    setTimeout(() => {
+      res.redirect('/result');
+    }, 2000);
+  } catch (error) {
+    console.log("Error");
+  }
 });
-app.get('/result', async function(req, res) {
-    //res.send(`Result: ${body1}`);
-    res.render("result.pug", {
-        query_result: q_res.body1,
-        email1: email,
-        phone1: phone
-    })
+app.get('/result', async function (req, res) {
+  //res.send(`Result: ${body1}`);
+  res.render("result.pug", {
+    query_result: q_res.body1,
+    email1: email,
+    phone1: phone
+  })
 });
-app.post('/result', async function(req, res) {
-    q_res.body1 = "";
-    email = req.body.email;
-    phone = req.body.phone;
-    console.log(email);
-    console.log(phone);
-    await search();
-    //I cant make redirect or render wait till im done with the api call
-    try {
-        //res.send(`Email: ${email} Phone: ${phone}`);
-        setTimeout(() => {
-            res.redirect('/result');
-        }, 2000);
-    } catch (error) {
-        console.log("Error");
-    }
+app.post('/result', async function (req, res) {
+  q_res.body1 = "";
+  email = req.body.email;
+  phone = req.body.phone;
+  console.log(email);
+  console.log(phone);
+  await search();
+  //I cant make redirect or render wait till im done with the api call
+  try {
+    //res.send(`Email: ${email} Phone: ${phone}`);
+    setTimeout(() => {
+      res.redirect('/result');
+    }, 2000);
+  } catch (error) {
+    console.log("Error");
+  }
 });
-app.listen(8080, function(err) {
-        console.log('Server started on port 8080....')
-    })
+
+app.get('/api/search', async function (req, res) {
+  email = req.query.email;
+  phone = req.query.phone;
+
+  const response = await axios.post(
+    // `/v2/combined?email=${email}&phone=${phone}/`
+    buildSearchUri(email, phone)
+  );
+
+  res.send(response.data['email.first_seen_anywhere']);
+});
+
+app.listen(8080, function (err) {
+  console.log('Server started on port 8080....')
+});
     //app.listen(443, function(err) {
     //   console.log('Server started on port 443....')
     //})
